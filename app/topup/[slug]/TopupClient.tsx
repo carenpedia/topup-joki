0550774278a2007700da34556984a51cd757f708
carenpedia <aryadimasp788@gmail.com>
2026-04-11 02:38:17 +0700

    fix ui sticky

--- app/topup/[slug]/TopupClient.tsx
+++ app/topup/[slug]/TopupClient.tsx
@@ -696,9 +696,8 @@ export default function TopupClient({
             >
               <div className="stickyBtnIcon">
                 <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
-                  <path d="M6 2L3 6v14a2 2 0 002 2h14a2 2 0 002-2V6l-3-4z"></path>
-                  <line x1="3" y1="6" x2="21" y2="6"></line>
-                  <path d="M16 10a4 4 0 01-8 0"></path>
+                  <path d="M6 8h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2z" />
+                  <path d="M16 10V6a4 4 0 0 0-8 0v4" />
                 </svg>
               </div>
               Pesan Sekarang!
